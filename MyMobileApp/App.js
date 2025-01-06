795aa21ce78356b04e884afcceaa78cb7c0fdf58
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-01-06 09:45:28 +0530

    Navigation is done

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import Courses from './components/Courses';
+import Subjects from './components/Subjects';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigateContainer>
+        <Stack.Navigator initialRouteName='login'>
+          <Stack.Screen name='login' component={Login} options={{title:'Login'}}/>
+          <Stack.Screen name='profile' component={Profile} options={{title:'Profile'}}/>
+          <Stack.Screen name='courses' component={Courses} options={{title:'Courses'}}/>
+          <Stack.Screen name='subjects' component={Subjects} options={{title:'Subjects'}}/>
+        </Stack.Navigator>
+
+      </NavigateContainer>
+    </SafeAreaProvider>
   );
 }
 
