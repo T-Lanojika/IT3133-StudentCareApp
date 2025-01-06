31d41efbcd70559a7b19facfbbb3e75303fba003
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-01-06 12:25:43 +0530

    login is updated for button

--- MyMobileApp/components/Login.js
+++ MyMobileApp/components/Login.js
@@ -1,7 +1,11 @@
-import { PaperProvider, TextInput } from "react-native-paper";
-import {  StyleSheet, Text, View, Image } from 'react-native';
+import { PaperProvider, TextInput, Button } from "react-native-paper";
+import {  StyleSheet, Text, View, Image} from 'react-native';
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Login(){
+    const navigation = useNavigation();
+    const [id,setId]=useState(null);
     return(
         <PaperProvider>
             <View style={styles.container}>
@@ -17,7 +21,13 @@ export default function Login(){
                         <Text style={styles.headingText}>STUDENT LOGIN</Text>
                     </View>  
                     <View style={styles.input}>
-                        <TextInput label={'Username'} ></TextInput>
+                        <TextInput label={'Username'} mode="outlined" value={id} onChangeText={setId}></TextInput>
+                    </View>
+                    <View style={styles.input}>
+                        <TextInput label={'Password'} mode="outlined"></TextInput>
+                    </View>
+                    <View style={styles.input}>
+                        <Button style={styles.buttonText} mode="outlined" onPress={()=>{navigation.navigate('profile')}}>Login</Button>
                     </View>                 
                 </View>
 
@@ -34,6 +44,7 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         backgroundColor: '#FFFFFF',
+        width:'100%',
         //flexDirection: 'column',
     },
     header: {
@@ -54,22 +65,31 @@ const styles = StyleSheet.create({
         backgroundColor: '#F4F6F',
         alignItems: 'center',
         justifyContent: 'center',
-        paddingHorizontal:20,
+        //paddingHorizontal:20,
       },
       imagepad:{
+        width: '100%', // Adjust size as needed
         alignItems:'center',
-        marginBottom: 20,
+        marginTop: 5,
       },
       image: {
-        width: 350, // Adjust size as needed
         height: 100,
         resizeMode: 'contain',
     },
     headingText:{
         textAlign:'center',
         fontSize:30,
-    }
-    ,
+        marginBottom:20,
+        marginTop:20,
+    },
+    input:{
+        marginBottom:10,
+    },
+    buttonText:{
+        color: '#FFFFFF',
+        backgroundColor:'#4b0150',
+    },
+
       footer: {
         flex: 0.5,
         backgroundColor: '#4b0150',
