0bb53124978d5e15a22643594a4c1b4a281e32d4
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-01-06 14:03:10 +0530

    login is completed

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -15,9 +15,9 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='login'>
+        <Stack.Navigator initialRouteName='login' style={styles.container}>
           <Stack.Screen name='login' component={Login} options={{headerShown:false}}/>
-          <Stack.Screen name='profile' component={Profile} options={{title:'Profile'}}/>
+          <Stack.Screen name='profile' component={Profile} options={{title:'UoV Student Care'}} style={styles.navigation}/>
           <Stack.Screen name='courses' component={Courses} options={{title:'Courses'}}/>
           <Stack.Screen name='subjects' component={Subjects} options={{title:'Subjects'}}/>
         </Stack.Navigator>
@@ -27,3 +27,13 @@ export default function App() {
   );
 }
 
+const styles = StyleSheet.create({
+  navigation:{
+    backgroundColor: '#4b0150',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#FFFFFF',
+    //fontSize: 24,
+    //fontWeight: 'bold',
+  }
+})
--- MyMobileApp/components/Profile.js
+++ MyMobileApp/components/Profile.js
@@ -1,6 +1,84 @@
-export default function Profile(){
+import { PaperProvider } from "react-native-paper";
+import {  StyleSheet, Text, View, Image} from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+
+export default function Profile({route}){
+    const navigation = useNavigation();
+
+    const detail = route.params;
     return(
-        <>
-        </>
+        <PaperProvider>
+            <View style={styles.container}>
+
+                <View style={styles.body}>
+                    <View style={styles.imagepad}>
+                        <Image source={require('../assets/profilepic/uovlogo.png')} style={styles.image}/>
+                    </View> 
+                </View> 
+
+                <View style={styles.cont}>
+                    <View></View>
+
+                </View>
+
+                <View style={styles.footer}>
+                    <Text style={styles.footerText}>UoV © Care</Text>
+                </View>
+
+            </View>
+            
+        </PaperProvider>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+        width:'100%',
+        //flexDirection: 'column',
+    },
+    header: {
+        flex: 1,
+        backgroundColor: '#4b0150',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 5,
+    },
+    headerText: {
+        color: '#FFFFFF',
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    body: {
+        flex: 5,
+        backgroundColor: '#F4F6F',
+        alignItems: 'center',
+        justifyContent: 'center',
+        //paddingHorizontal:20,
+      },
+      imagepad:{
+        width: '100%', // Adjust size as needed
+        alignItems:'center',
+        marginTop: 5,
+      },
+      image: {
+        height: 100,
+        resizeMode: 'contain',
+    },
+
+      footer: {
+        flex: 0.5,
+        backgroundColor: '#4b0150',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      footerText:{
+        color:'#FFFFFF',
+        fontSize:12,
+        textAlign:'center',
+      }
+
+  });
+  
